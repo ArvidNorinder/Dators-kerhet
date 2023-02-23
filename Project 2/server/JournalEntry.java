37f0c0ef099cd361290aa1aaf2bf8ebff5cf63bf
hjalmarsand <hjalmarsandblom@gmail.com>
2023-02-23 13:27:36 +0100

    JournalEntryParser.java now has proper functionality for read and write to journalEntries.txt

--- Project 2/server/JournalEntry.java
+++ Project 2/server/JournalEntry.java
@@ -7,17 +7,16 @@ public class JournalEntry {
     private String division;
     private String patientID;
 
-    public JournalEntry(String date, String doc, String nurse, String div, String id, String patientID) {
-        this.date = date;
+    public JournalEntry( String patientID, String doc, String nurse, String div, String date) {
+        this.patientID = patientID;
         this.doctor = doc;
         this.nurse = nurse;
         this.division = div;
-        this.patientID = patientID;
+        this.date = date;
       }
 
-
-    public String getDate() {
-        return date;
+    public String getpatientID() {
+        return patientID; 
     }
 
     public String getDoctor() {
@@ -32,7 +31,7 @@ public class JournalEntry {
         return division;
     }
 
-    public String getpatientID() {
-        return patientID; 
+    public String getDate() {
+        return date;
     }
 }
--- Project 2/server/JournalEntryParser.java
+++ Project 2/server/JournalEntryParser.java
@@ -3,34 +3,71 @@ package server;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.List;
-import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class JournalEntryParser { 
-    private File file; 
-    //The format of the record filelines is: recordID;date;Patient;Doctor;Nurse;Division
+    private File fileName; 
+    //The format of the record filelines is: Date;Patient;Doctor;Nurse;Division
     //Note: Should the entries contain any information about the patient?
-    private FileReader reader;
+
 
     public JournalEntryParser(String file) throws FileNotFoundException {
-        this.reader = new FileReader(file);
-        this.file = new File(file);
+        fileName = new File(file);
     }
 
     public static void main(String[] args) {
         try {
-            JournalEntryParser parser = new JournalEntryParser("./database/journal.txt");
+            JournalEntryParser parser = new JournalEntryParser("./database/journalEntries.txt");
+            JournalEntry entry = new JournalEntry("Arvid", "Sofia", "Niklas", "division1", "2023:02:23");
+            parser.write(entry);
+            Map<String, List<JournalEntry>> patientEntries = parser.read();
+            System.out.println(patientEntries);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public Map<String, List<JournalEntry>> read() {
-        return null;
-    }
+    //Returns a map with the patients as keys and a list of all journal entries as values
+    public Map<String, List<JournalEntry>> read() throws FileNotFoundException { 
+        Map<String, List<JournalEntry>> patientEntries = new HashMap<String, List<JournalEntry>>();
+        List <JournalEntry> entries = new ArrayList<JournalEntry>();
 
-    public void write() {
+        Scanner myReader = new Scanner(fileName);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            String[] entry = data.split(";");
+            JournalEntry journalEntry = new JournalEntry(entry[0], entry[1], entry[2], entry[3], entry[4]);
+            entries.add(journalEntry);
+        }
+        myReader.close();
 
+        for (JournalEntry entry : entries) {
+            if (patientEntries.containsKey(entry.getpatientID())) {
+                patientEntries.get(entry.getpatientID()).add(entry);
+            } else {
+                List<JournalEntry> entryList = new ArrayList<JournalEntry>();
+                entryList.add(entry);
+                patientEntries.put(entry.getpatientID(), entryList);
+            }
+        }
+        return patientEntries;
+
+        }
+
+    
+
+    public void write(JournalEntry entry) { //Writes a JournalEntry to the file
+        try {
+
+            PrintWriter writer = new PrintWriter(new FileWriter(fileName, true));
+            writer.append(entry.getpatientID() + ";" + entry.getDoctor() + ";" + entry.getNurse() + ";" + entry.getDiv() + ";" + entry.getDate() + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
